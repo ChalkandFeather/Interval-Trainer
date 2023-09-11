7f683889e1054a92231ab043f2b91ec31690afa2
marie taylor <marietaylortailor@hotmail.com>
2023-09-11 15:56:52 +0100

    stripped back to basics.Requires some functionality updates

--- IntervalTrainerMVP/script.js
+++ IntervalTrainerMVP/script.js
@@ -16,27 +16,23 @@ function formatCounter(milliseconds) {
     .toString()
     .padStart(2, "0")}`;
 
-  // Debugging: Log the formatted time
-  // console.log("Formatted Time:", formattedTime);
-
-  // return formattedTime;
-}
-function updateCounterDisplay() {
-  const currentTime = Date.now();
-  const elapsedTime = currentTime - startTime - pausedTime;
-  const formattedTime = formatCounter(elapsedTime);
-
-  const timerDisplayElement = document.getElementById("timerDisplay");
-  if (timerDisplayElement) {
-    timerDisplayElement.textContent = formattedTime;
-  }
+  return formattedTime;
 }
 
 function startCounter() {
   if (!isPaused) {
     startTime = Date.now() - pausedTime; // Adjust start time if not paused
   }
-  intervalId = setInterval(updateCounterDisplay, 100); // Update the counter display
+  intervalId = setInterval(function () {
+    const currentTime = Date.now();
+    const elapsedTime = currentTime - startTime;
+    const formattedTime = formatCounter(elapsedTime);
+
+    const timerDisplayElement = document.getElementById("timerDisplay");
+    if (timerDisplayElement) {
+      timerDisplayElement.textContent = formattedTime;
+    }
+  }, 100); // Update the counter every 100 milliseconds
   isPaused = false;
 }
 
@@ -44,17 +40,15 @@ function pauseCounter() {
   clearInterval(intervalId);
   pausedTime = Date.now() - startTime;
   isPaused = true;
-  updateCounterDisplay(); // Update the counter display immediately when paused
 }
 
 function stopCounter() {
   clearInterval(intervalId);
   pausedTime = 0;
   isPaused = false;
-  updateCounterDisplay(); // Update the counter display immediately when paused
 }
 
-// Event listeners for the "Start" and "Stop" buttons
+// Event listeners for the "Start," "Pause," and "Stop" buttons
 const startButton = document.getElementById("startButton");
 const pauseButton = document.getElementById("pauseButton");
 const stopButton = document.getElementById("stopButton");
