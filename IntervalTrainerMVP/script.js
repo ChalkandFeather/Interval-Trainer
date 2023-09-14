b287f57713f7f7471a68b2b2489e96fc7b70fc94
marie taylor <marietaylortailor@hotmail.com>
2023-09-14 13:38:47 +0100

    notated explanations for partial code

--- IntervalTrainerMVP/script.js
+++ IntervalTrainerMVP/script.js
@@ -1,8 +1,9 @@
-let startTime;
-let intervalId;
-let pausedTime = 0;
-let isPaused = false;
+let startTime; // This line initializes a variable called "startTime" to keep track of when the timer started.
+let intervalId; // This variable will store the ID of the interval timer that updates the display.It will retain this ID until one of the following actions is taken Reset. effectively cancelling the timer. Afterwards, intervalId will be cleared, and the timer will no longer update until you start it again.
+let pausedTime = 0; // This variable keeps track of the time when the timer was paused.
+let isPaused = false; // This variable tells us whether the timer is currently paused or not.
 
+// This function formats the time in milliseconds into a readable format.
 function formatCounter(milliseconds) {
   const totalMilliseconds = Math.floor(milliseconds);
   const hours = Math.floor(totalMilliseconds / 3600000);
@@ -18,12 +19,16 @@ function formatCounter(milliseconds) {
 
   return formattedTime;
 }
-
+// This function starts or resumes the timer.
 function startCounter() {
+  // Check if the timer is not paused.
   if (!isPaused) {
+    // Calculate the time when the timer starts or resumes.
     startTime = Date.now() - pausedTime; // Adjust start time if not paused
   }
+  // Set up an interval timer to update the display every 100 milliseconds.
   intervalId = setInterval(function () {
+    // Calculate the elapsed time since the timer started or resumed.
     const currentTime = Date.now();
     const elapsedTime = currentTime - startTime;
     const formattedTime = formatCounter(elapsedTime);
@@ -33,14 +38,20 @@ function startCounter() {
       timerDisplayElement.textContent = formattedTime;
     }
   }, 100); // Update the counter every 100 milliseconds
+  // The timer is not paused anymore.
   isPaused = false;
 }
 
 function pauseCounter() {
+  // Step 1: Clear the interval timer associated with intervalId.
   clearInterval(intervalId);
+  // Step 2: Calculate how much time has passed since the timer started.
+  // `Date.now()` gives the current time in milliseconds, and `startTime` is when the timer started.
   pausedTime = Date.now() - startTime;
+  // Step 3: Set isPaused to true.
+  // This indicates that the timer is now paused.
   isPaused = true;
-}
+} //no need to update timerDisplayElement as this is unchanged whilst paused
 
 function stopCounter() {
   clearInterval(intervalId);
